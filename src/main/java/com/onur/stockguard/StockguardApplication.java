6dc7fdc1d77fd12672f84b71c8305ddb3e4e36dd
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2026-02-12 21:09:20 +0300

    Initialize the project

---
+++ src/main/java/com/onur/stockguard/StockguardApplication.java
@@ -0,0 +1,13 @@
+package com.onur.stockguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onur/stockguard/StockguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onur.stockguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
